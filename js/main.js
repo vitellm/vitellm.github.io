b7da2f307afd0685bc26f433c527641dec875aed
vitellm <vitellofellow1@gmail.com>
2017-11-22 23:11:25 -0600

    pre-release

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var x = 0;
+var images = [];
+images[0] = "img/artwork.jpg";
+images[1] = "img/artwork2.jpg";
+images[2] = "img/artwork3.jpg";
+images[3] = "img/artwork4.jpg";
+
+function changeImage()
+{
+
+    var img = document.getElementById('art');
+    img.src = images[x];
+    x++;
+    console.log(x);
+    if(x >= images.length){
+        x = 0;
+    } 
+    setTimeout("changeImage()", 3500);
+}
+
+
+
+$( document ).ready(function() {
+    changeImage();
+});
