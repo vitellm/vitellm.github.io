f21d9a461e024cd55c3800d2f99c9cf6ea67e334
vitellm <vitellofellow1@gmail.com>
2017-12-05 21:28:39 -0600

    added logo

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 $(function() {
-    // setTimeout() function will be fired after page is loaded
-    // it will wait for 5 sec. and then will fire
-    // $("#successMessage").hide() function
+
     setTimeout(function() {
         $(".wrapper").hide('blind', {}, 11000)
     }, 110000);
+
+    setTimeout(function() {
+        $("#cath").hide('blind', {}, 296)
+    }, 2960);
 });
