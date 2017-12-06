66d44251c58d8194c4fa1b1fe9c2036645f63b6c
vitellm <vitellofellow1@gmail.com>
2017-12-05 22:57:43 -0600

    add js for mobile

--- js/main.js
+++ js/main.js
@@ -8,3 +8,14 @@ $(function() {
         $("#cath").hide('blind', {}, 290)
     }, 2900);
 });
+
+document.addEventListener('touchstart', this.touchstart);
+document.addEventListener('touchmove', this.touchmove);
+
+function touchstart(e) {
+    e.preventDefault()
+}
+
+function touchmove(e) {
+    e.preventDefault()
+}
