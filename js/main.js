265b2f3610147b55bcb3cd4358eff53a298292d3
mattvitello <vitellofellow1@gmail.com>
2017-12-06 10:21:40 -0600

    slowed to song

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 $(function() {
 
+/*
     setTimeout(function() {
         $(".wrapper").hide('blind', {}, 16000)
     }, 160000);
-
+*/
     setTimeout(function() {
-        $("#cath").hide('blind', {}, 479)
-    }, 4790);
+        $("#cath").hide('blind', {}, 475)
+    }, 4750);
 });
 
 //document.addEventListener('touchstart', this.touchstart);
