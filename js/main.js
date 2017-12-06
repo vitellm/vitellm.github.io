204e7dc2b24a9cf2bd0cd3e141145edee6270c4e
vitellm <vitellofellow1@gmail.com>
2017-12-05 23:02:21 -0600

    js modify

--- js/main.js
+++ js/main.js
@@ -9,12 +9,10 @@ $(function() {
     }, 2900);
 });
 
-document.addEventListener('touchstart', this.touchstart);
+//document.addEventListener('touchstart', this.touchstart);
 document.addEventListener('touchmove', this.touchmove);
 
-function touchstart(e) {
-    e.preventDefault()
-}
+
 
 function touchmove(e) {
     e.preventDefault()
