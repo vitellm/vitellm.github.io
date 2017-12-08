0c2bff8a05d9cf2794dd5c9922eccfa433a0941e
mattvitello <vitellofellow1@gmail.com>
2017-12-07 21:52:56 -0600

    new vid test

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-$("#vid1").on("loadstart", function () {
-	console.log("loaded vid")
-	setTimeout(function() {
-        $("#vid1").hide('blind', {}, 21300)
-    }, 213000);
-});
 
 $(function() {
 
@@ -12,6 +6,14 @@ $(function() {
         $(".wrapper").hide('blind', {}, 16000)
     }, 160000);
 */
+	var video = document.getElementById('vid1');
+
+	video.addEventListener('loadeddata', function() {
+  		console.log("loaded vid");
+  		setTimeout(function() {
+        	$("#vid1").hide('blind', {}, 21300)
+    	}, 213000);
+	}, false);
 
     setTimeout(function() {
         $("#cath").hide('blind', {}, 475)
